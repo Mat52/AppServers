52296da6405c54b5ba9fa2c64fb77de728fb4cfe
Krzysztof Sliwinski <skrzysiek16.9@gmail.com>
2021-06-15 09:38:00 +0200

    loading naprawiony

--- src/static/game.js
+++ src/static/game.js
@@ -12,10 +12,15 @@ import Main from "./components/Main";
 
 async function init() {
   const container = document.getElementById("root");
+  document.getElementById("rightscore").style.display = "none";
   let loaderFBX = new LoaderFBX();
   console.log(`Przed await`);
   await loaderFBX.loadModel();
   document.getElementById("loadingScreen").remove();
+  let timeout = setTimeout(() => {
+    document.getElementById("rightscore").style.display = "block";
+  }, 1000);
+  await timeout;
   console.log(`Po awaicie`);
   //main class object
   new Main(container, loaderFBX.model);
